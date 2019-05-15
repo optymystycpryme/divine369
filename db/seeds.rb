9a33f1571edb1aee16e800eb7e0dbae923b75d30
brian haltom <brianwh87@gmail.com>
2019-05-15 14:34:35 -0700

    Updated registration forms with names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: 'Pellentesque sit amet eros mi. Sed elementum arcu et elit fringilla, eu accumsan magna laoreet. Etiam a consequat neque. Donec pellentesque vel enim sed auctor. Fusce hendrerit ipsum eu tempus suscipit. Etiam luctus dui et porttitor pellentesque. Aliquam ut sapien eget eros suscipit eleifend ac ac sapien. Nunc ac tempor purus, commodo semper odio.'
+  )
+end
